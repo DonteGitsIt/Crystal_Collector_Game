acd04b6ed63a59bcafbd26d5f1571e1b5075694e
Donte Ojeda <donte.ojeda@gmail.com>
2018-09-08 15:22:35 -0400

    finished game

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+
+$(document).ready(function() {
+    var wins = 0
+    var losses = 0
+    var comNumber = Math.floor(Math.random() * 120) + 19
+    var currentTotal = 0
+    var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    var winsText = document.getElementById("wins")
+    var lossesText = document.getElementById("losses")
+    var comNumberText = document.getElementById("comNumber")
+    var totalText = document.getElementById("current-total")
+    var congratsText = document.getElementById("congrats-text")
+
+    winsText.textContent = wins
+    lossesText.textContent = losses
+    comNumberText.textContent = comNumber
+    totalText.textContent = currentTotal
+    
+    
+    // function that need to be run everytime user wins or loses.
+    function newGame(){
+        comNumber = Math.floor(Math.random() * 120) + 19
+        currentTotal = 0
+        comNumberText.textContent = comNumber
+        totalText.textContent = currentTotal
+
+        // assigns a random value from crystalNumbers array to each crystal button
+        $(".crystalButton").each(function(){
+            var randomValue = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)]
+            $(this).attr("value", randomValue)
+        })
+
+    }
+    //adds value of clicked crystal button to users total score
+    $(".crystalButton").on('click', function(){
+        var valueString = ''
+        valueString = $(this).attr("value")
+        valueString = parseInt(valueString)
+        currentTotal = currentTotal + valueString
+        totalText.textContent = currentTotal
+        //checks if user won or lost with each click of a crystal
+        if(currentTotal === comNumber){
+            wins++
+            winsText.textContent = wins
+            congratsText.textContent = ("You won!!")
+            newGame()
+        }else if(currentTotal > comNumber){
+            losses++
+            lossesText.textContent = losses
+            congratsText.textContent = ("You lose!!")
+            newGame()
+        }
+    })
+
+    newGame()
+})
+
+
+
